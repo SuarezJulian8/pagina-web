4bf1b05ae8ff5e3fabfc174595446005edca6c90
SuarezJulian8 <130808190+SuarezJulian8@users.noreply.github.com>
2024-05-02 19:53:09 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd 
+import pickle as pl 
+import numpy as np 
+st.title("Prediccion de los precios para casas")
+data = pd.read_csv("hprice.csv")
+with open("Lr.pickle", "rb") as f:
+    Lr = pl.load(f)
+#Widgest para la entrada de datos 
+lotsize = st.slider("Tamaño del lote (lotsize)", float(data["lotsize"].min()), float(data["lotsize"].max()), float(data["lotsize"].mean()))
+assess = st.slider("assess", float(data["assess"].min()), float(data["assess"].max()), float(data["assess"].mean()))
+sqrft = st.slider("Pies cuadrados (sqrft)", int(data["sqrft"].min()), int(data["sqrft"].max()), int(data["sqrft"].mean()))
+bdrms = st.slider("Numero de habitaciones (bdrms)", int(data["bdrms"].min()), int(data["bdrms"].max()), int(data["bdrms"].mean()))
+colonial = st.radio("¿Es colonial?", ("Si", "No"))
+
+print("lotsize:", lotsize)
+print("assess:", assess)
+print("colonial_binary:", colonial)
+print("sqrft:", sqrft)
+print("bdrms:", bdrms)
+
+colonial_binary = 1 if colonial == "Si" else 0 
+valores=np.array([[lotsize,assess,colonial_binary,sqrft,bdrms]])
+p=Lr.predict([[lotsize,assess,colonial_binary,sqrft,bdrms]])
+st.write(st.write("El valor del inmueble es " + str(p)))
+
+
